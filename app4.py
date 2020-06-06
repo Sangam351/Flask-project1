03003217f429281ce5fb9aeee309e697f6bbd4ee
Author Sushil Shakya <sushe.shakya@gmail.com>
2020-06-06 13:24:04 +0545

    Add jwt_required for all the APIs

--- app4.py
+++ app4.py
@@ -1,22 +1,30 @@
 from flask import Flask,request
 from flask_restful import Api , Resource
 from flask_jwt import JWT ,jwt_required , current_identity
-from security import auntheticate , identity
+from security import authenticate , identity
 
 app = Flask(__name__)
 app.secret_key = "sangam"
 api = Api(app)
-jwt = JWT(app,auntheticate,identity)
+jwt = JWT(app, authenticate, identity)
 
 items = []
 
+
+@app.route("/protected")
+@jwt_required()
+def protected():
+    return f"{current_identity}"
+
+
 class Item(Resource):
-    @jwt_required
-    def get(self,nam):
+    @jwt_required()
+    def get(self,nam=None):
         item = next(filter(lambda x:x['name'] == nam, items), None)
         return {'item':item} ,200 if item else 404
 
-    def post(self,nam):
+    @jwt_required()
+    def post(self,nam=None):
         if next(filter(lambda x:x['name'] == nam,items),None):
             return{'message':"an item with name '{}' already exist." .format(nam)}
         data = request.get_json()
@@ -25,12 +33,14 @@ class Item(Resource):
         items.append(item)
         return item,201
 
-    def delete(self,nam):
+    @jwt_required()
+    def delete(self,nam=None):
         global items
         items = list(filter(lambda x:x['name'] != nam,items))
         return{'message':'item deleted'}
 
-    def put(self,nam):
+    @jwt_required()
+    def put(self,nam=None):
         data = request.get_json()
         item = next(filter(lambda x:x['name'] == nam,items),None)
         if item is None:
@@ -41,9 +51,10 @@ class Item(Resource):
         return item
 
 class item_list(Resource):
+    @jwt_required()
     def get(self):
-        return {'itmes':items}
+        return {'items':items}
 
-api.add_resource(Item,'/item/<string:nam>')
+api.add_resource(Item,'/item/<nam>')
 api.add_resource(item_list,'/items')
 app.run(port = 5000 , debug = True)
